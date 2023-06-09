99a74772bdf8c4e709419717765c475fc4ec4132
shahKrinal <123356601+shahKrinal@users.noreply.github.com>
2023-06-09 14:06:37 +0530

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 a = {"a": [1, 2, 3, 4, 5, 6], "b": [3, 4, 5, 6, 7, 8]}
 df = pd.DataFrame(a)
-df.to_csv(".github/workspace/output.csv", index=False)
+df.to_csv("output.csv", index=False)
